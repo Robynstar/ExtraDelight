d5acc09845cf839846f96b062c0651803eb76388
Lance5057 <Lance5057@gmail.com>
2025-01-08 20:11:43 -0600

    2.2.2

--- src/main/java/com/lance5057/extradelight/ExtraDelight.java
+++ src/main/java/com/lance5057/extradelight/ExtraDelight.java
@@ -21,7 +21,7 @@ import net.neoforged.neoforge.common.NeoForgeMod;
 @Mod(ExtraDelight.MOD_ID)
 public class ExtraDelight {
 	public final static String MOD_ID = "extradelight";
-	public static final String VERSION = "2.2.1";
+	public static final String VERSION = "2.2.2";
 
 	public static Logger logger = LogManager.getLogger();
 
